c43be3ea829bf256a1103164948566e96ae34bf0
IMAGE113 <95086452+IMAGE113@users.noreply.github.com>
2026-04-19 21:09:44 +0630

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,8 +1,15 @@
-
 from pydantic import BaseModel
+
 
 class ProductCreate(BaseModel):
     name: str
     price: float
     stock: int
     code: str
+
+
+class OrderCreate(BaseModel):
+    product_id: int
+    qty: int
+    total: float
+    payment_type: str
