e45cb29d911cf860c75d0cd737ab1d4f5890c817
IMAGE113 <95086452+IMAGE113@users.noreply.github.com>
2026-04-28 18:17:56 +0630

    Update telegram.py

--- app/services/telegram.py
+++ app/services/telegram.py
@@ -1,1 +1,17 @@
+import httpx
 
+http_client = httpx.AsyncClient(timeout=10)
+
+async def send(token, chat_id, text, reply_markup=None):
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+
+    payload = {
+        "chat_id": int(chat_id),
+        "text": str(text),
+        "parse_mode": "Markdown"
+    }
+
+    if reply_markup:
+        payload["reply_markup"] = reply_markup
+
+    await http_client.post(url, json=payload)
