0ecf8788c212689a82e2620c076264f2bd4e5606
IMAGE113 <95086452+IMAGE113@users.noreply.github.com>
2026-04-28 18:17:26 +0630

    Update database.py

--- app/db/database.py
+++ app/db/database.py
@@ -1,1 +1,52 @@
+import asyncpg
+from app.core.config import DATABASE_URL
 
+pool = None
+
+async def get_db_pool():
+    global pool
+    if not pool:
+        pool = await asyncpg.create_pool(DATABASE_URL)
+    return pool
+
+async def init_db(pool):
+    async with pool.acquire() as conn:
+        await conn.execute("""
+        CREATE TABLE IF NOT EXISTS businesses (
+            id SERIAL PRIMARY KEY,
+            name TEXT,
+            tg_bot_token TEXT UNIQUE
+        );
+        CREATE TABLE IF NOT EXISTS products (
+            id SERIAL PRIMARY KEY,
+            business_id INTEGER REFERENCES businesses(id) ON DELETE CASCADE,
+            name TEXT,
+            price INTEGER
+        );
+        CREATE TABLE IF NOT EXISTS task_queue (
+            id SERIAL PRIMARY KEY,
+            business_id INTEGER,
+            chat_id BIGINT,
+            user_text TEXT,
+            request_hash TEXT UNIQUE,
+            status TEXT DEFAULT 'pending',
+            created_at TIMESTAMP DEFAULT NOW()
+        );
+        CREATE TABLE IF NOT EXISTS pending_orders (
+            chat_id BIGINT,
+            business_id INTEGER,
+            order_data TEXT,
+            PRIMARY KEY(chat_id, business_id)
+        );
+        CREATE TABLE IF NOT EXISTS orders (
+            id SERIAL PRIMARY KEY,
+            business_id INTEGER,
+            chat_id BIGINT,
+            customer_name TEXT,
+            phone_no TEXT,
+            address TEXT,
+            payment_method TEXT,
+            items TEXT,
+            total_price INTEGER
+        );
+        """)
